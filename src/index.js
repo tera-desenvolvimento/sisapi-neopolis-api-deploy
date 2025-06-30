56bbff7865313a5a38c2ecdd775a432b134fd980
Suporte Tera <suporte@tera.tec.br>
2025-06-30 20:24:15 -0300

    Ajustando headers

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     app.use(CORS());
     next();
 })
