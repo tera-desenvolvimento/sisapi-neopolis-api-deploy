ef15fd284f3be6c6e68f42ae471b686f6e8d14f1
Suporte Tera <suporte@tera.tec.br>
2025-08-29 20:03:32 -0300

    Criando rota de notificação de transporte

---
+++ src/controllers/trip/notifyPacient.controller.js
@@ -0,0 +1,35 @@
+const tripModel = require('../../models/trip.model');
+const axios = require('axios');
+
+async function notifyPacient(tripId, patientNumber, patientName) {
+    try {
+        const tripData = await tripModel.findById(tripId);
+        const endpoint = process.env.WHAPI_BOT_URL + "/sendMessage";
+
+        let messageData = `📢 Este é o canal oficial de notificações da Secretaria Municipal de Saúde de Neópolis.\n\nOlá, ${patientName}! Tenho uma mensagem aqui do nosso setor de transportes👋\n\nTô passando pra confirmar sua vaga no dia: ${tripData.date} no veículo das ${tripData.exitTime} com destino à ${tripData.destination}! 🚐\n\nÉ importante lembrar que o veículo sai no horário marcado, seja pontual.\n\n📍 Rua do Bonfim, nº 565 – Neópolis\n\n🕒 Segunda a sexta, das 7h30 às 13h\n\n⚠️ Este número envia apenas notificações e não recebe mensagens.\n\nEm caso de dúvidas, procure diretamente a Secretaria. 😊`;
+
+        const sendMessage = await axios.post(endpoint, {
+            headers: {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                to: patientNumber,
+                message: messageData
+            }
+        });
+
+        return {
+            status: 200,
+            message: "EXAME_NOTIFICATION_SUCCESS",
+            data: sendMessage.data
+        }
+    } catch (error) {
+        return {
+            status: 200,
+            message: "EXAME_NOTIFICATION_FAILED",
+            error: error.message
+        }
+    }
+}
+
+module.exports = notifyPacient;
--- src/routes/trip.routes.js
+++ src/routes/trip.routes.js
@@ -6,6 +6,7 @@ const updateTrip = require('../controllers/trip/updateTrip.controller');
 const removeTrip = require('../controllers/trip/removeTrip.controller');
 const addPatient = require('../controllers/trip/addPatient.controller');
 const findTrip = require('../controllers/trip/findTrip.controller');
+const notifyPacient = require('../controllers/trip/notifyPacient.controller');
 
 router.post('/trip/create', async (req, res) => {
     const { date } = req.body;
@@ -42,6 +43,12 @@ router.post('/trip/addPatient/:id', async (req, res) => {
     const { id } = req.params;
     const { patientData } = req.body;
     const result = await addPatient(id, patientData);
+    res.json(result);
+});
+
+router.post('/trip/notifyPatient', async (req, res) => {
+    const { tripId, patientNumber, patientName } = req.body;
+    const result = await notifyPacient(tripId, patientNumber, patientName);
     res.json(result);
 });
 
